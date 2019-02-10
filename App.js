86338d0cb75e56a7b270b75796975adf773e305a
bportman <benjamin@orpheusincorporated.com>
2019-02-10 14:41:03 -0500

    basic recipe search

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, Image, Button, Linking } from 'react-native';
 
 export default class App extends React.Component {
+  state = { 
+    search: '',
+    results: []
+  };
+
+  // api key a6777c00068f36f86fc5c379d021b86b 
+
+  lookupRecipes() {
+    var baseUrl = 'https://www.food2fork.com/api/'
+    var endpoint = 'search'
+    var apiKey = 'a6777c00068f36f86fc5c379d021b86b'
+    var query = this.state.search
+
+    fetch(baseUrl + endpoint + '?key=' + apiKey + '&q=' + query)
+        .then(res => res.json())
+        .then((myJson) => {
+          this.setState({
+            results: myJson.recipes
+          })
+        })
+        .catch(err => console.log(err))
+  }
+
+  viewRecipe = (url) => {
+    Linking.openURL(url)
+  }
+
+  _renderItem = ({item}) => {
+      return (
+        <View style={styles.recipe}>
+          <Image
+            style={{width: 250, height: 250}}
+            source={{uri: item.image_url}}
+          />
+          <Text>{item.title}</Text>
+          <Button
+            onPress={() => this.viewRecipe(item.source_url)}
+            title="View Recipe"
+            color="#841584"
+          />
+        </View>
+        )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to Munch Recipes</Text>
+        <TextInput
+        style={styles.search}
+        onChangeText={(search) => this.setState({search})}
+        onEndEditing={() => this.lookupRecipes()}
+        value={this.state.search}
+        placeholder="Try 'Avocado' or 'Chicken,Quinoa'..."
+      />
+      <FlatList
+        data={this.state.results}
+        renderItem={this._renderItem}
+      />
       </View>
     );
   }
@@ -14,8 +69,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  search: {
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 3,
+    width: '90%'
+  },
+  recipe: {
+    flex: 1,
+    marginTop: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
